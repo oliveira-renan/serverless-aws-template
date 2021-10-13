7daa381f38a026c57a27ceb15517dff03ed509c3
Renan Rangel <renan.rangel@olxbr.com>
2021-10-13 19:59:26 -0300

    fix(api): add queue url with env param

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,20 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as serverless from 'serverless-http';
+import { SQS } from 'aws-sdk';
 
 const app = express();
+const sqs = new SQS();
 
 app.use(bodyParser.json({ strict: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+
+  sqs.sendMessage({
+    MessageBody: req.body,
+    QueueUrl: process.env.QUEUE_URL,
+  });
 });
 
 // eslint-disable-next-line import/prefer-default-export
